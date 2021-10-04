be3d547dc750fb616da6146c9121f865fd0898a7
leny32 <40176071+yezzl@users.noreply.github.com>
2021-10-04 05:28:52 +0200

    there u go

--- bot.js
+++ bot.js
@@ -10,4 +10,18 @@ client.on('chat', (channel, user, message, self) => {
   if ((message.indexOf('!')) !== -1) {
     commandResolver.resolve(channel, user, message)
   }
-})
+});
+
+client.on("submysterygift", (channel, username, numbOfSubs, methods, userstate) => {
+  for (let i = 0; i < numbOfSubs; i++) {
+    client.say(channel, "Clap Clap");
+  }
+});
+
+client.on("subgift", (channel, username, streakMonths, recipient, methods, userstate) => {
+  client.say(channel, "Clap Clap");
+});
+
+client.on("subscribers", (channel, enabled) => {
+  client.say(channel, "Clap Clap");
+});
--- commands.js
+++ commands.js
@@ -23,21 +23,51 @@ const callCommand = (command, messageInfo) => {
   state = messageInfo;
 
   switch (command.command) {
-    case 'to':
-      timeOutUser(command.args)
-      break
     case 'clear':
       clear();
       break
-    case 'fb':
-      client.action(activeChannel, config.get('social.facebook'))
-      break
-    case 'twt':
-      client.action(activeChannel, config.get('social.twitter'))
-      break
+    case 'hvorlenge':
+      client.say(activeChannel, `Streamen startet 03.10.2021 17:30 og har vært gående i\n${time()}`);
+      break;
+    case 'sek':
+      client.say(activeChannel, `Streamen har vært gående i: ${dtime()} sekunder (${parseInt(dtime() / 60)} minutter)`);
+      break;
     default:
       break
   }
+}
+
+const startDate = new Date("10/03/2021 17:28:00");
+
+const dtime = () => {
+  return parseInt(Math.abs(startDate - new Date()) / 1000);
+}
+
+const time = () => {
+
+  let t = "";
+  // get total seconds between the times
+  let delta = Math.abs(startDate - new Date()) / 1000;
+
+  // calculate (and subtract) whole days
+  let days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+  t += days > 1 ? days + " dager, " : days > 0 ? "1 dag, " : "";
+
+  // calculate (and subtract) whole hours
+  let hours = Math.floor(delta / 3600) % 24;
+  delta -= hours * 3600;
+  t += hours > 1 ? hours + " timer, " : hours > 0 ? "1 time, " : "";
+
+  // calculate (and subtract) whole minutes
+  let minutes = Math.floor(delta / 60) % 60;
+  delta -= minutes * 60;
+  t += minutes > 1 ? minutes + " minutter og " : "1 minutt og ";
+
+  // what's left is seconds
+  let seconds = parseInt(delta % 60);
+  t += seconds > 1 ? seconds + " sekunder." : "1 sekund";
+  return t;
 }
 
 const checkModeratorPermission = () => state.user.mod || state.user.username === activeChannel
