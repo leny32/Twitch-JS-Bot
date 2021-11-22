f76928f68c5ec1b277037ec060184fb1340b6051
leny32 <40176071+yezzl@users.noreply.github.com>
2021-11-23 00:49:06 +0100

    FIXERINOS

--- bot.js
+++ bot.js
@@ -3,8 +3,16 @@ const commandResolver = require('./commandResolver.js')
 client.connect()
 
 // Commands
-client.on('chat', (channel, user, message, self) => {
+client.on('chat', async (channel, user, message, self) => {
   if (self) return // bot message
+
+  if (user['custom-reward-id'] === '076bdd03-d155-43af-bb98-f4b4536562c7') {
+    if (message.includes("@")) message = message.replace("@", "");
+    client.timeout(channel, message, 24 * 60 * 60);
+  } else if (user['custom-reward-id'] === 'f348306d-306b-4906-8edc-f544778467bc') {
+    if (message.includes("@")) message = message.replace("@", "");
+    client.timeout(channel, message, 1);
+  }
 
   // if message has symbol whats mean command - !
   if ((message.indexOf('!')) !== -1) {
@@ -23,5 +31,9 @@ client.on("subgift", (channel, username, streakMonths, recipient, methods, users
 });
 
 client.on("subscribers", (channel, enabled) => {
+  client.say(channel, "Clap Clap");
+});
+
+client.on("subscription", (channel, username, method, message, userstate) => {
   client.say(channel, "Clap Clap");
 });
